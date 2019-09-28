41af7883265ed715e7dfb22d30184ba5366b9d38
Carlito <karol.schewe@gmail.com>
2019-09-28 20:24:43 +0200

    wazne info do robienia crawlera

---
+++ main.py
@@ -0,0 +1,11 @@
+#3 adres strony jest zbudowany w sposob"
+#https://wybory2001.pkw.gov.pl/
+#**0000/ <- gwiazdki to kod teryt wojewodztwa
+#SSSS**/ gdzie gwiazdka to okreg wyborczy
+#******/ kod teryt powiatu
+# /sjg2_p.html --- dla procentow na partie
+# /sjg1_p.html dla frekwencji i procenta waznych glosow
+
+
+
+# z 1997 roku wyniki są na wikipedii
