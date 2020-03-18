4f507799252b47de8036e4846359745b90097e2e
Carlito <karol.schewe@gmail.com>
2020-03-18 11:30:50 +0100

    attached crawled data to git;added crawler for 2005

---
+++ crawler2005.py
@@ -0,0 +1,83 @@
+import pandas as pd
+from time import sleep
+import requests
+
+
+
+
+
+
+def crawl_2005(teryt_code_list: pd.DataFrame):
+    '''
+
+        :param teryt_code_list: dataframe containing teryt code list (look example teryt file in repository)
+        :return: tuple of dataframes containing:
+        sejm_df - sejm 2007 elections outcome
+        turnout_df - 2007 elections turnout
+    '''
+    year = 2007
+
+    turnout_list = []
+
+    # WYBORY DO SENATU W TEJ ORDYNACJI WYBORCZEJ POZWALAŁY NA ODDANIE GŁOSU NA DOWOLNĄ LICZBĘ KANDYDATÓW
+    # -- nie przyda mi sie to wiec na razie
+    sejm_list = []
+    for ind, code in teryt_code_list.iterrows():
+        teryt = code['TERYT']
+        powiat = code['Powiat']
+        gmina_name = code['Gmina']
+        url = "https://wybory2005.pkw.gov.pl/SJM/PL/WYN/W/" + teryt + ".htm"
+        print(url)
+
+        for i in range(3):
+            try:
+                site = requests.get(url, timeout=5)
+                break
+            except requests.exceptions.ConnectTimeout:
+                print("timeout error")
+                sleep(5)
+
+        try:
+            url_data = pd.read_html(site.content)
+        except ValueError:
+            print("nie ma takiej gminy")
+            continue
+
+        # w tabelce z indeksem cztery - frekwencja
+
+        for index, row in url_data[5].iterrows():
+            if row[0] == 'Σ':
+                break
+            df_row = {
+                'year': year,
+                'elections': "sejm",
+                'teryt_code': teryt,
+                'powiat': powiat,
+                'gmina': gmina_name,
+                'political_party': row[3],
+                'n_votes': row[4],
+                'percentage': row[5]
+            }
+
+            sejm_list.append(df_row)
+
+        turnout_row = {
+            'year': year,
+            'teryt_code': teryt,
+            'powiat': powiat,
+            'gmina': gmina_name,
+            'population': url_data[4][1][2],
+            'eligible_to_vote': url_data[4][1][6].replace('\xa0', ''),
+            'turnout_%': url_data[4][1][12][:-1],
+            'turnout_ppl': url_data[4][1][8].replace('\xa0', '')
+        }
+        turnout_list.append(turnout_row)
+
+    sejm_df = pd.DataFrame.from_dict(sejm_list)
+    turnout_df = pd.DataFrame.from_dict(turnout_list)
+
+    return sejm_df, turnout_df
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from crawler_2011 import crawl_2011
 from crawler_2007 import crawl_2007
+from crawler2005 import crawl_2005
 #author: Karol Schewe
 '''
 Do uruchomienia tego programu potrzebny jest plik z lista kodów TERYT gmin, o których zamierzamy zebrać informacje.
@@ -18,19 +19,20 @@ Program zapisuje wyniki wyborów do sejmu, senatu oraz frekwencję na poziomie g
 
 
 teryt_code_list = pd.read_excel('terytki.xls', header=0, converters={'TERYT': str})
-sejm_df, turnout_df, senat_df = crawl_2011(teryt_code_list[0:10])
-sejm_df_2007, turnout_df_2007 = crawl_2007(teryt_code_list[0:10])
+# sejm_df, turnout_df, senat_df = crawl_2011(teryt_code_list[0:10])
+# sejm_df_2007, turnout_df_2007 = crawl_2007(teryt_code_list)
+sejm_df_2005, turnout_df_2005 = crawl_2005(teryt_code_list)
 
-print(sejm_df.to_string())
-
-
-sejm_df.to_csv("wybory_do_sejmu_2011_po_gminach_wyniki.csv", index=False, encoding='utf-8-sig')
-turnout_df.to_csv("frekwencja_wybory_2011_po_gminach.csv", index=False, encoding='utf-8-sig')
-senat_df.to_csv("wybory_do_senatu_2011_po_gminach_wyniki.csv", index=False, encoding='utf-8-sig')
+# print(sejm_df.to_string())
 
 
-sejm_df.to_csv("wybory_do_sejmu_2007_po_gminach_wyniki.csv", index=False, encoding='utf-8-sig')
-turnout_df.to_csv("frekwencja_wybory_2007_po_gminach.csv", index=False, encoding='utf-8-sig')
+# sejm_df.to_csv("wybory_do_sejmu_2011_po_gminach_wyniki.csv", index=False, encoding='utf-8-sig')
+# turnout_df.to_csv("frekwencja_wybory_2011_po_gminach.csv", index=False, encoding='utf-8-sig')
+# senat_df.to_csv("wybory_do_senatu_2011_po_gminach_wyniki.csv", index=False, encoding='utf-8-sig')
+
+
+sejm_df_2005.to_csv("wybory_do_sejmu_2005_po_gminach_wyniki.csv", index=False, encoding='utf-8-sig')
+turnout_df_2005.to_csv("frekwencja_wybory_2005_po_gminach.csv", index=False, encoding='utf-8-sig')
 
 
 
